9a7b9e8cd3cb26e424a1ed5dcf794862ff031e30
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-18 16:53:31 +0100

    Added custom exception handling for 404 and 405

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from config import Config
 from routers import routers
 import logging
@@ -21,3 +22,31 @@ for router in routers:
 def on_startup():
     logger.info(f"Documentation can be found at the {app.docs_url} "
                 f"or {app.redoc_url} endpoints.")
+
+
+@app.exception_handler(404)
+async def custom_404_handler(_, __):
+    return HTMLResponse("""
+        <!DOCTYPE html>
+        <html>
+        <title>Not Found</title>
+        <body>
+            <h1>Not Found</h1>
+            <p>The requested resource was not found on this server.</p>
+        </body>
+        </html>
+    """)
+
+
+@app.exception_handler(405)
+async def custom_405_handler(_, __):
+    return HTMLResponse("""
+        <!DOCTYPE html>
+        <html>
+        <title>Not Allowed</title>
+        <body>
+            <h1>Not Allowed</h1>
+            <p>The requested resource does not allow that method.</p>
+        </body>
+        </html>
+    """)
