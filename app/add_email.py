29a916cd4e4a1642e0146b6d32b76cb4554b0a4b
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-18 16:53:47 +0100

    Added template for add_email_to_queue function

---
+++ app/add_email.py
@@ -0,0 +1,12 @@
+def add_email_to_queue(email_request):
+    """
+    Adds an email request object to the FIFO Queue.
+
+        Parameters:
+            email_request (Email): An email object from the /email endpoint. 
+                                   Please see models/reqest_models/email for the definition
+
+        Returns:
+            status_code (int): The status code after attempting to add the email_request to the FIFO Queue.   
+    """
+    pass
