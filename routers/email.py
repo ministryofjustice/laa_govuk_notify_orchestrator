ba757e17af147f852a57fddee71baecf5c9ebf07
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-19 12:56:35 +0100

    Improved endpoint documentation

--- routers/email.py
+++ routers/email.py
@@ -2,14 +2,22 @@ from fastapi.routing import APIRouter
 from models.request_models.email import Email
 from fastapi import BackgroundTasks
 from app.add_email import add_email_to_queue
+from docs.routers.email_router import EmailRouter
 
 
 email_router = APIRouter()
 
 
-@email_router.post("/email", summary="Send email to GOV.UK Notify", description="Will queue your email to be sent to GOV.UK Notify,\
-                    if their service is down the email will be held in an encrypted queue until a response is recieved.\
-                    If after two weeks no response is recieved your email be deleted.", status_code=201)
+@email_router.post("/email", summary=EmailRouter.summary, description=EmailRouter.description, status_code=201)
 async def send_email(email_request: Email, background_tasks: BackgroundTasks):
+    """
+    API Endpoint for /email.
+    Recieves an email object and loads it onto the FIFO Queue. 
+    A 422 response is sent if the email_request object is found to be invalid by pydantic
+    A 201 response is sent if the email_request object is valid
+        Parameters:
+            email_request (Email): An email object from the /email endpoint.
+                                   Please see models/reqest_models/email for the definition
+    """
     background_tasks.add_task(add_email_to_queue, email_request)
     return
