905102e021d631dcd02163c0de7e8cb25dcda642
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-19 14:03:31 +0100

    Changed location of routing documentation

--- routers/email.py
+++ routers/email.py
@@ -2,7 +2,7 @@ from fastapi.routing import APIRouter
 from models.request_models.email import Email
 from fastapi import BackgroundTasks
 from app.add_email import add_email_to_queue
-from docs.routers.email_router import EmailRouter
+from routers.docs.email_router import EmailRouter
 
 
 email_router = APIRouter()
@@ -12,9 +12,10 @@ email_router = APIRouter()
 async def send_email(email_request: Email, background_tasks: BackgroundTasks):
     """
     API Endpoint for /email.
-    Recieves an email object and loads it onto the FIFO Queue. 
+    Recieves an email object and loads it onto the FIFO Queue.
     A 422 response is sent if the email_request object is found to be invalid by pydantic
     A 201 response is sent if the email_request object is valid
+
         Parameters:
             email_request (Email): An email object from the /email endpoint.
                                    Please see models/reqest_models/email for the definition
