03fc358a0a26b22db9f2b80bf281df73ff87d1a8
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-09-29 15:47:33 +0100

    Improved documentation

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export ENVIRONMENT=${1:-development}
+
+# COMPOSE_PROFILES is used to set the docker compose profile
+# If this is set to development then RabbitMQ will launch with the API
 export COMPOSE_PROFILES=$ENVIRONMENT
+
 echo "running environment $ENVIRONMENT"
+
 docker-compose down --remove-orphans
 docker-compose up
