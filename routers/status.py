d33fb9671655e5d80bfc4c17a1369cd082fff260
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-18 13:11:42 +0100

    Replaced celery queue check with worker check. Reenabled deployment probes.

--- routers/status.py
+++ routers/status.py
@@ -1,5 +1,4 @@
 from fastapi.routing import APIRouter
-from utils.celery import get_celery_status
 from celery import current_app as celery_app
 
 
@@ -10,6 +9,6 @@ status_router = APIRouter()
 def read_status():
     """
     API Endpoint for "/status".
-    Returns the celery worker status including the active queue
+    Returns 200 if the service is alive.
     """
-    return get_celery_status(celery_app)
+    return celery_app.control.inspect().active()
