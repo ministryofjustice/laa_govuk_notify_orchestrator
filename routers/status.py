76c5b9fc99e2e95ed3dcfb42b00f2e883e1433bb
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-18 13:23:38 +0100

    Redefined status endpoint

--- routers/status.py
+++ routers/status.py
@@ -1,5 +1,4 @@
 from fastapi.routing import APIRouter
-from celery import current_app as celery_app
 
 
 status_router = APIRouter()
@@ -11,4 +10,5 @@ def read_status():
     API Endpoint for "/status".
     Returns 200 if the service is alive.
     """
-    return celery_app.control.inspect().active()
+    response = {"status": "ok"}
+    return response
