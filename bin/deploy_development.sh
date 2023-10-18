61ce797eabfde70666efd5ed0f4977f1cfeb08f1
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-18 12:03:34 +0100

    Added values-development.yaml

--- bin/deploy_development.sh
+++ bin/deploy_development.sh
@@ -7,7 +7,7 @@ HELM_DIR="$ROOT/../helm_deploy/laa-govuk-notify-orchestrator/"
 helm upgrade laa-govuk-notify-orchestrator \
   $HELM_DIR \
   --namespace=$NAMESPACE \
-  --values ${HELM_DIR}/values.yaml \
+  --values ${HELM_DIR}/values-development.yaml \
   --set image.repository=$REGISTRY_HOST \
   --set image.tag=$CIRCLE_SHA1 \
   --install
