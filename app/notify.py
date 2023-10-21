c9fb4e9327a6cbafba9efcdd988b7a3d0b94b0f5
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-21 02:35:15 +0100

    Added error handling to creating the Notify API Client

--- app/notify.py
+++ app/notify.py
@@ -5,11 +5,20 @@ import logging
 
 logger = logging.getLogger("uvicorn")
 
-notifications_client = NotificationsAPIClient(Config.GOVUK_NOTIFY_API_KEY)
+try:
+    notify_client = NotificationsAPIClient(Config.GOVUK_NOTIFY_API_KEY)
+except AssertionError as exception:
+    if not Config.TESTING_MODE:
+        raise exception
+    notify_client = None
 
 
-def send_email(email_address: str, template_id: str, personalisation: dict):
-    response = notifications_client.send_email_notification(
+def send_email(self, email_address: str, template_id: str, personalisation: dict):
+    if not notify_client:
+        raise EnvironmentError(
+            "Notify client has not been created. Please ensure you have a valid Notify API Key set as GOVUK_NOTIFY_API_KEY"
+        )
+    response = self.api_client.send_email_notification(
         email_address=email_address,  # required string
         template_id=template_id,  # required UUID string
         personalisation=personalisation,  # optional dictionary
