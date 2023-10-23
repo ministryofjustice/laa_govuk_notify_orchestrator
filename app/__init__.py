56048d9ee6e983de8b26c5ca8d9b8d5302f8b90c
said-moj <45761276+said-moj@users.noreply.github.com>
2023-10-23 15:03:29 +0100

    Add sentry integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,20 @@
 import logging
 from app.main import create_app
+from config import Config
+import sentry_sdk
 
+if Config.SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=Config.SENTRY_DSN,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=0.1,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=0.1,
+        environment=Config.ENVIRONMENT,
+    )
 
 logger = logging.getLogger("uvicorn")
 notify_orchestrator_api = create_app()
