070f7ce2d72e845721da64a0922ec343caadd3a8
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-11-22 16:42:18 +0000

    Added HTTP 429 Error check

--- app/tasks/email.py
+++ app/tasks/email.py
@@ -1,6 +1,7 @@
 from celery import current_app as app
 from config import Config
 from app.email import Email
+import datetime as dt
 from notifications_python_client.errors import HTTPError, TokenError
 import logging
 
@@ -8,6 +9,9 @@ logger = logging.getLogger("uvicorn")
 
 
 class EmailTask(app.Task):
+    rate_limit_exceeded = False
+    datetime_rate_limit_resets = None
+
     def retry_email(self, email: Email, exception: Exception):
         """
         This increments the retry_count and calls a retry on the task with the exception that caused the retry passed in.
@@ -53,15 +57,42 @@ class EmailTask(app.Task):
 
         MAX_RETRY_TIME_SECONDS = 7200  # 2 Hours
 
-        retry_time = 5 * (2 ** email.retry_count)  # 10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120
+        retry_time = 5 * (2**email.retry_count)  # 10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120
 
         return retry_time if retry_time < MAX_RETRY_TIME_SECONDS else MAX_RETRY_TIME_SECONDS
+
+    @staticmethod
+    def is_exception_http_429(exception: Exception) -> bool:
+        if not isinstance(exception, HTTPError):
+            return False
+        try:
+            return exception.status_code == 429
+        except KeyError:
+            return False
+        return False
+
+    @staticmethod
+    def get_datetime_of_next_midnight():
+        now = dt.datetime.now()
+        midnight = dt.datetime.min.time()
+        today_midnight = dt.datetime.combine(now, midnight)
+        return today_midnight + dt.timedelta(days=1)
 
 
 @app.task(bind=True, base=EmailTask)
 def email_task(self, email: Email):
     try:
+        if EmailTask.rate_limit_exceeded:
+            if dt.now() > EmailTask.datetime_rate_limit_resets:
+                EmailTask.rate_limit_exceeded = False
+            else:
+                raise RuntimeError(
+                    "Rate limit has been exceeded, will not attempt to send any more emails until midnight."
+                )
         email.send_email()
     except Exception as exception:
+        if EmailTask.is_exception_http_429(exception):
+            EmailTask.rate_limit_exceeded = True
+            EmailTask.datetime_rate_limit_resets = EmailTask.get_datetime_of_next_midnight()
         self.log_error_message(exception)
         self.retry_email(email, exception)
