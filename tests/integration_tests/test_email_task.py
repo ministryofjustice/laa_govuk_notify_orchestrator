01455d91ec940f7b0c0dca4144ae303a77bd27b8
said-moj <45761276+said-moj@users.noreply.github.com>
2026-06-17 15:04:49 +0100

    Do not reraise 400 errors (#90)

--- tests/integration_tests/test_email_task.py
+++ tests/integration_tests/test_email_task.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from app.email import Email
 from models.request_models.email import Email as EmailRequest
@@ -26,3 +26,19 @@ class TestSendEmail:
 
         email_task(email)
         retry_email.assert_called()
+
+    @patch("app.email.Email.send_email")
+    @patch("app.tasks.email.EmailTask.retry_email")
+    @patch("app.tasks.email.EmailTask.log_error_message")
+    def test_400_response(self, log_error_message, retry_email, send_email):
+        """Do not retry email after receiving 400 error from notification API"""
+
+        email_request = EmailRequest.model_validate(TestData.valid_email_request)
+        email = Email(email_request)
+        http_error = HTTPError()
+        http_error.error = MagicMock(status_code=400)
+        send_email.side_effect = http_error
+        email_task(email)
+
+        retry_email.assert_not_called()
+        log_error_message.assert_not_called()
